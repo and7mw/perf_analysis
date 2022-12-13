cf87d3371a00ebbb967fef24cbdad93dbea21810
and7mw <and7mw@gmail.com>
2022-12-13 22:05:51 +0300

    accuracy passed

--- conv.cpp
+++ conv.cpp
@@ -28,6 +28,10 @@ void conv_nchw(const float *input,
         for (size_t k_ic = 0; k_ic < wShape[1]; k_ic++) {
             for (size_t kh = 0; kh < wShape[2]; kh++) {
                 for (size_t kw = 0; kw < wShape[3]; kw++) {
+                    // std::cout << "k_ic: " << k_ic << ", "
+                    //           << "kh: " << kh << ", "
+                    //           << "kw: " << kw << std::endl;
+
                     const float inVal = inDataB[k_ic * inChStride + offset_in_each_ch + kh * inShape[3] + kw];
                     const float wVal = weight[oc * wOutChStride + k_ic * wInChStride + kh * wShape[3] + kw];
 
@@ -35,6 +39,7 @@ void conv_nchw(const float *input,
                 }
             }
         }
+        // std::cout << "result: " << result << std::endl;
         outDataB[oc * outShape[2] * outShape[3] + oh * outShape[3] + ow] = result;
     };
     
@@ -59,15 +64,45 @@ void conv_nchw(const float *input,
     }
 }
 
+void accuracy_test();
+
 int main() {
+    accuracy_test();
+
+    // const size_t N = 1;
+    // const size_t C_IN = 32;
+    // const size_t C_OUT = 32;
+
+    // const size_t IH = 64; const size_t IW = 64;
+
+    // const size_t KH = 3;
+    // const size_t KW = 3;
+
+    // const size_t OH = IH - KH + 1;
+    // const size_t OW = IW - KW + 1;
+
+    // shape inShape{N, C_IN, IH, IW};
+    // shape wShape{C_OUT, C_IN, KH, KW};
+    // shape outShape{N, C_OUT, OH, OW};
+
+    // std::vector<float> input(inShape[0] * inShape[1] * inShape[2] * inShape[3]);
+    // std::vector<float> weight(wShape[0] * wShape[1] * wShape[2] * wShape[3]);
+    // std::vector<float> output(outShape[0] * outShape[1] * outShape[2] * outShape[3]);
+
+    // conv_nchw(input.data(), weight.data(), output.data(), inShape, wShape, outShape);
+
+    return 0;
+}
+
+void accuracy_test() {
     const size_t N = 1;
-    const size_t C_IN = 32;
-    const size_t C_OUT = 32;
+    const size_t C_IN = 8;
+    const size_t C_OUT = 1;
 
-    const size_t IH = 64; const size_t IW = 64;
+    const size_t IH = 4; const size_t IW = 4;
 
-    const size_t KH = 3;
-    const size_t KW = 3;
+    const size_t KH = 2;
+    const size_t KW = 2;
 
     const size_t OH = IH - KH + 1;
     const size_t OW = IW - KW + 1;
@@ -76,11 +111,35 @@ int main() {
     shape wShape{C_OUT, C_IN, KH, KW};
     shape outShape{N, C_OUT, OH, OW};
 
+    // per each channel
+    //
+    // [ 1, 2, 3, 1
+    //   2, 1, 1, 4
+    //   3, 2, 3, 1
+    //   1, 1, 1, 1 ]
+    const std::vector<float> inPerCh{1, 2, 3, 1, 2, 1, 1, 4, 3, 2, 3, 1, 1, 1, 1, 1};
     std::vector<float> input(inShape[0] * inShape[1] * inShape[2] * inShape[3]);
+
+    // per each channel
+    //
+    // [ 2, 3
+    //   1, 2 ]
+    const std::vector<float> wPerCh{2, 3, 1, 2};
     std::vector<float> weight(wShape[0] * wShape[1] * wShape[2] * wShape[3]);
+
     std::vector<float> output(outShape[0] * outShape[1] * outShape[2] * outShape[3]);
+    std::vector<float> outRef{96.0f, 128.0f, 144.0f, 112.0f, 104.0f, 152.0f, 120.0f, 128.0f,  96.0f};
+
+    for (size_t i = 0; i < (inShape[0] * inShape[1]); i++) {
+        std::copy(inPerCh.begin(), inPerCh.end(), input.begin() + i * (inShape[2] * inShape[3]));
+        std::copy(wPerCh.begin(), wPerCh.end(), weight.begin() + i * (wShape[2] * wShape[3]));
+    }
 
     conv_nchw(input.data(), weight.data(), output.data(), inShape, wShape, outShape);
 
-    return 0;
+    for (size_t i = 0; i < outRef.size(); i++) {
+        if (std::abs(outRef[i] - output[i]) > 0.0001f) {
+            throw std::runtime_error("Accuracy error!");
+        }
+    }
 }
