7d34771efa2df4518436b50bf28be522acde8e1a
and7mw <and7mw@gmail.com>
2022-12-13 20:52:27 +0300

    ref done, valgrind passed, accuracy not tested

--- conv.cpp
+++ conv.cpp
@@ -1,33 +1,58 @@
 #include <iostream>
+#include <array>
+#include <vector>
 
-// [N, C_IN, Z, Y, X]
-// [C_OUT, C_IN, Z, Y, X]
-// [N, C_OUT, Z, Y, X]
+using shape = std::array<size_t, 4>;
 
-// input: [N, C, H, W]
-// output: [N, KN, H - KH + 1, W - KW + 1]
+// input shape: [N, C_IN, H, W]
+// weight shape: [C_OUT, C_IN, H, W]
+// output shape: [N, C_OUT, H, W]
+
 void conv_nchw(const float *input,
                const float *weight,
                float *output,
-               const size_t N,
-               const size_t C,
-               const size_t H,
-               const size_t W,
-               const size_t KN,
-               const size_t KC,
-               const size_t KH,
-               const size_t KW) {
-    assert(C == KC)
+               const shape& inShape,
+               const shape& wShape,
+               const shape& outShape) {
+    auto kernel = [&](const float* inDataB,
+                      float* outDataB,
+                      const size_t offset_in_each_ch,
+                      const size_t oc,
+                      const size_t oh,
+                      const size_t ow) {
+        const size_t inChStride = inShape[2] * inShape[3];
+        const size_t wInChStride = wShape[2] * wShape[3];
+        const size_t wOutChStride = wShape[1] * wShape[2] * wShape[3];
 
-    const size_t ON = N; const size_t OC = KN;
-    const size_t OH = H - KH + 1;
-    const size_t OW = W - KW + 1;
+        float result = 0.0f;
+        for (size_t k_ic = 0; k_ic < wShape[1]; k_ic++) {
+            for (size_t kh = 0; kh < wShape[2]; kh++) {
+                for (size_t kw = 0; kw < wShape[3]; kw++) {
+                    const float inVal = inDataB[k_ic * inChStride + offset_in_each_ch + kh * inShape[3] + kw];
+                    const float wVal = weight[oc * wOutChStride + k_ic * wInChStride + kh * wShape[3] + kw];
 
-    for (size_t on = 0; on < ON; on++) {
-        for (size_t oc = 0; oc < OC; oc++) {
-            for (size_t oh = 0; oh < OH; oh++) {
-                for (size_t ow = 0; ow < OW; ow++) {
-        
+                    result += inVal * wVal;
+                }
+            }
+        }
+        outDataB[oc * outShape[2] * outShape[3] + oh * outShape[3] + ow] = result;
+    };
+    
+    const size_t inBatchStride = inShape[1] * inShape[2] * inShape[3];
+    const size_t outBatchStride = outShape[1] * outShape[2] * outShape[3];
+    for (size_t on = 0; on < outShape[0]; on++) {
+        for (size_t oc = 0; oc < outShape[1]; oc++) {
+            for (size_t oh = 0; oh < outShape[2]; oh++) {
+                for (size_t ow = 0; ow < outShape[3]; ow++) {
+                    // std::cout << "on: " << on << ", "
+                    //           << "oc: " << oc << ", "
+                    //           << "oh: " << oh << ", "
+                    //           << "ow: " << ow << std::endl;
+
+                    const size_t offset_in_each_ch = oh * inShape[3] + ow;
+
+                    kernel(input + on * inBatchStride, output + on * outBatchStride,
+                           offset_in_each_ch, oc, oh, ow);
                 }
             }
         }
@@ -35,5 +60,27 @@ void conv_nchw(const float *input,
 }
 
 int main() {
+    const size_t N = 1;
+    const size_t C_IN = 32;
+    const size_t C_OUT = 32;
+
+    const size_t IH = 64; const size_t IW = 64;
+
+    const size_t KH = 3;
+    const size_t KW = 3;
+
+    const size_t OH = IH - KH + 1;
+    const size_t OW = IW - KW + 1;
+
+    shape inShape{N, C_IN, IH, IW};
+    shape wShape{C_OUT, C_IN, KH, KW};
+    shape outShape{N, C_OUT, OH, OW};
+
+    std::vector<float> input(inShape[0] * inShape[1] * inShape[2] * inShape[3]);
+    std::vector<float> weight(wShape[0] * wShape[1] * wShape[2] * wShape[3]);
+    std::vector<float> output(outShape[0] * outShape[1] * outShape[2] * outShape[3]);
+
+    conv_nchw(input.data(), weight.data(), output.data(), inShape, wShape, outShape);
+
     return 0;
 }
